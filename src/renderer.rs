c57fdc28da046f895c266c76df6dccdf9390867e
Irjan Olsen <irjan.olsen@gmail.com>
2023-09-11 21:20:25 +0200

    Improved font loading using font-kit crate.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,5 +1,8 @@
 extern crate sdl2;
 
+use std::path::PathBuf;
+use font_kit::handle::Handle;
+use font_kit::source::SystemSource;
 use sdl2::Sdl;
 use sdl2::ttf::Sdl2TtfContext;
 use sdl2::ttf::Font;
@@ -14,13 +17,22 @@ pub struct TermRenderer<'a> {
 }
 
 impl<'a> TermRenderer<'a> {
+    fn get_font_path(name: &str) -> Option<PathBuf> {
+        match SystemSource::new().select_by_postscript_name(name).ok()? {
+            Handle::Path { path, .. } => Some(path),
+            Handle::Memory { .. } => None,
+        }
+    }
+
     pub fn new(sdl_context: &'a Sdl, ttf_context: &'a Sdl2TtfContext) -> TermRenderer<'a> {
+        let hack_regular = Self::get_font_path("Hack-Regular").unwrap();
+        let hack_bold = Self::get_font_path("Hack-Bold").unwrap();
         Self {
             sdl_context,
             ttf_context,
             //font: ttf_context.load_font("/usr/share/fonts/TTF/DroidSansMono.ttf", 15).unwrap(),
-            font: ttf_context.load_font("/usr/share/fonts/TTF/Hack-Regular.ttf", 15).unwrap(),
-            bold_font: ttf_context.load_font("/usr/share/fonts/TTF/Hack-Bold.ttf", 15).unwrap(),
+            font: ttf_context.load_font(hack_regular, 15).unwrap(),
+            bold_font: ttf_context.load_font(hack_bold, 15).unwrap(),
             video_subsystem: sdl_context.video().unwrap(),
         }
     }
